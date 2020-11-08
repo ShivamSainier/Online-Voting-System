a4e5219c2c5e41884c716ef3803d81f7bd311562
shivam saini <59383963+shivam007-ss@users.noreply.github.com>
2020-11-08 11:43:43 +0530

    create forms

--- forms.py
+++ forms.py
@@ -6,14 +6,14 @@ from flask_wtf.file import FileField
 
 
 class admin_form(FlaskForm):
-    Email=StringField('E-mail',validators=[DataRequired()])
-    Password=PasswordField("Password",validators=[DataRequired()])
+    Email=StringField('E-mail',validators=[DataRequired(),Length(min=3,max=50)])
+    Password=PasswordField("Password",validators=[DataRequired(),Length(min=5,max=10)])
     Submit=SubmitField("Submit")
 
 class candidate(FlaskForm):
-    Name=StringField('Candidate name',validators=[DataRequired()])
+    Name=StringField('Candidate name',validators=[DataRequired(),Length(min=3,max=50)])
     Voter_id=StringField('Candidate Id',validators=[DataRequired()])
-    address=TextAreaField('Address',validators=[DataRequired()])  
+    address=TextAreaField('Address',validators=[DataRequired(),Length(min=10,max=50)])  
     cat = RadioField('Gender:', choices = ['Male', 'Female'])
     phone=IntegerField('Phone No',validators=[DataRequired(),Length(min=10,max=12)])
     picture=FileField('Add Picture')
